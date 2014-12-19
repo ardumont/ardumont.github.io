78df9cdf2c1fdbf07c7408fc1f91582c2ff4ee00
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-12-19 21:17:46 +0100

    Rename function to org2jekyll setup

--- publish.el
+++ publish.el
@@ -34,36 +34,37 @@
 
 (require 'org)
 
-(defvar source-directory     nil "Path to the source directory.")
-(defvar jekyll-directory     nil "Path to Jekyll blog.")
-(defvar jekyll-drafts-dir    nil "Relative path to drafts directory.")
-(defvar jekyll-posts-dir     nil "Relative path to posts directory.")
-(defvar jekyll-post-ext      nil "File extension of Jekyll posts.")
-(defvar jekyll-org-post-template nil "Default template for org2jekyll posts.
+(defvar org2jekyll/source-directory         nil "Path to the source directory.")
+(defvar org2jekyll/jekyll-directory         nil "Path to Jekyll blog.")
+(defvar org2jekyll/jekyll-drafts-dir        nil "Relative path to drafts directory.")
+(defvar org2jekyll/jekyll-posts-dir         nil "Relative path to posts directory.")
+(defvar org2jekyll/jekyll-post-ext          nil "File extension of Jekyll posts.")
+(defvar org2jekyll/jekyll-org-post-template nil
+  "Default template for org2jekyll posts.
 %s will be replace by the post title.")
 
-(setq source-directory     (expand-file-name "~/org")
-      jekyll-directory     (expand-file-name "~/public_html")
-      jekyll-drafts-dir    "_drafts/"
-      jekyll-posts-dir     "_posts/"
-      jekyll-post-ext      ".org"
-      jekyll-org-post-template "#+STARTUP: showall\n#+STARTUP: hidestars\n#+OPTIONS: H:2 num:nil tags:nil toc:1 timestamps:t\n#+LAYOUT: post\n#+TITLE: %s\n#+DESCRIPTION: \n#+CATEGORIES:\n\n* ")
+(setq org2jekyll/source-directory         (expand-file-name "~/org")
+      org2jekyll/jekyll-directory         (expand-file-name "~/public_html")
+      org2jekyll/jekyll-drafts-dir        "_drafts/"
+      org2jekyll/jekyll-posts-dir         "_posts/"
+      org2jekyll/jekyll-post-ext          ".org"
+      org2jekyll/jekyll-org-post-template "#+STARTUP: showall\n#+STARTUP: hidestars\n#+OPTIONS: H:2 num:nil tags:nil toc:1 timestamps:t\n#+LAYOUT: post\n#+TITLE: %s\n#+DESCRIPTION: \n#+CATEGORIES:\n\n* ")
 
-(defun input-directory (&optional folder-name)
+(defun org2jekyll/input-directory (&optional folder-name)
   "Compute the input folder from the FOLDER-NAME."
-  (format "%s/%s" source-directory (if folder-name folder-name "")))
+  (format "%s/%s" org2jekyll/source-directory (if folder-name folder-name "")))
 
-(defun output-directory (folder-name)
+(defun org2jekyll/output-directory (folder-name)
   "Compute the output folder from the FOLDER-NAME."
-  (format "%s/%s" jekyll-directory folder-name))
+  (format "%s/%s" org2jekyll/jekyll-directory folder-name))
 
 ;; org setup for publishing blog
 (custom-set-variables '(org-publish-project-alist
                         `(("org"
-                           :base-directory ,(input-directory)
+                           :base-directory ,(org2jekyll/input-directory)
                            :base-extension "org"
                            ;; :publishing-directory "/ssh:user@host:~/html/notebook/"
-                           :publishing-directory ,(output-directory "blog")
+                           :publishing-directory ,(org2jekyll/output-directory "blog")
                            :publishing-function org-html-publish-to-html
                            :headline-levels 4
                            :section-numbers nil
@@ -76,9 +77,9 @@
                            :body-only t)
 
                           ("tony-blog"
-                           :base-directory ,(input-directory)
+                           :base-directory ,(org2jekyll/input-directory)
                            :base-extension "org"
-                           :publishing-directory ,(output-directory "_posts")
+                           :publishing-directory ,(org2jekyll/output-directory "_posts")
                            :publishing-function org-html-publish-to-html
                            :headline-levels 4
                            :section-numbers nil
@@ -91,61 +92,61 @@
                            :body-only t)
 
                           ("images"
-                           :base-directory ,(input-directory "img")
+                           :base-directory ,(org2jekyll/input-directory "img")
                            :base-extension "jpg\\|gif\\|png"
-                           :publishing-directory ,(output-directory "img")
+                           :publishing-directory ,(org2jekyll/output-directory "img")
                            :publishing-function org-publish-attachment)
 
                           ("js"
-                           :base-directory ,(input-directory "js")
+                           :base-directory ,(org2jekyll/input-directory "js")
                            :base-extension "js"
-                           :publishing-directory ,(output-directory "js")
+                           :publishing-directory ,(org2jekyll/output-directory "js")
                            :publishing-function org-publish-attachment)
 
                           ("other"
-                           :base-directory ,(input-directory "css")
+                           :base-directory ,(org2jekyll/input-directory "css")
                            :base-extension "css\\|el"
-                           :publishing-directory ,(output-directory "css")
+                           :publishing-directory ,(org2jekyll/output-directory "css")
                            :publishing-function org-publish-attachment)
 
                           ("website" :components ("org" "tony-blog" "images" "js" "css")))))
 
-(defun jekyll-make-slug (s)
+(defun org2jekyll/--make-slug (s)
   "Turn a string S into a slug."
   (replace-regexp-in-string
    " " "-" (downcase
             (replace-regexp-in-string
              "[^A-Za-z0-9 ]" "" s))))
 
-(defun jekyll-yaml-escape (s)
+(defun org2jekyll/--yaml-escape (s)
   "Escape a string S for YAML."
   (if (or (string-match ":" s)
           (string-match "\"" s))
       (concat "\"" (replace-regexp-in-string "\"" "\\\\\"" s) "\"")
     s))
 
-(defun jekyll-draft-post (title)
+(defun org2jekyll/create-draft-post (title)
   "Create a new Jekyll blog post with TITLE."
   (interactive "sPost Title: ")
-  (let ((draft-file (concat jekyll-directory jekyll-drafts-dir
-                            (jekyll-make-slug title)
-                            jekyll-post-ext)))
+  (let ((draft-file (concat org2jekyll/jekyll-directory org2jekyll/jekyll-drafts-dir
+                            (org2jekyll/--make-slug title)
+                            org2jekyll/jekyll-post-ext)))
     (if (file-exists-p draft-file)
         (find-file draft-file)
       (progn (find-file draft-file)
-             (insert (format jekyll-org-post-template (jekyll-yaml-escape title)))))))
+             (insert (format org2jekyll/jekyll-org-post-template (org2jekyll/--yaml-escape title)))))))
 
-(defun jekyll-list-posts ()
+(defun org2jekyll/list-posts ()
   "Lists the posts folder."
   (interactive)
-  (find-file (output-directory jekyll-posts-dir)))
+  (find-file (org2jekyll/output-directory org2jekyll/jekyll-posts-dir)))
 
-(defun jekyll-list-drafts ()
+(defun org2jekyll/list-drafts ()
   "Lists the drafts folder."
   (interactive)
-  (find-file (output-directory jekyll-drafts-dir)))
+  (find-file (org2jekyll/output-directory org2jekyll/jekyll-drafts-dir)))
 
-(defun publish/get-option! (opt)
+(defun org2jekyll/get-option-at-point! (opt)
   "Gets the header value of the option OPT from a buffer."
   (let* ((regexp (org-make-options-regexp (list (upcase opt) (downcase opt)))))
     (save-excursion
@@ -153,15 +154,15 @@
       (if (re-search-forward regexp nil t 1)
           (match-string-no-properties 2)))))
 
-(defun publish/get-option-from-file! (orgfile option)
+(defun org2jekyll/get-option-from-file! (orgfile option)
   "Return the ORGFILE's OPTION."
   (with-temp-buffer
     (when (file-exists-p orgfile)
       (insert-file-contents orgfile)
       (goto-char (point-min))
-      (publish/get-option! option))))
+      (org2jekyll/get-option-at-point! option))))
 
-(defun publish/get-options-from-file! (orgfile options)
+(defun org2jekyll/get-options-from-file! (orgfile options)
   "Return the ORGFILE's OPTIONS."
   (with-temp-buffer
     (when (file-exists-p orgfile)
@@ -169,32 +170,32 @@
       (mapcar (lambda (option)
                 (save-excursion
                   (goto-char (point-min))
-                  (cons option (publish/get-option! option))))
+                  (cons option (org2jekyll/get-option-at-point! option))))
               options))))
 
-(defun publish/article-p! (orgfile)
+(defun org2jekyll/article-p! (orgfile)
   "Determine if the current ORGFILE is an article or not.
 Depends on the metadata header blog."
-  (publish/get-option-from-file! orgfile "BLOG"))
+  (org2jekyll/get-option-from-file! orgfile "BLOG"))
 
-(defun publish/get-publish-date! (orgfile)
+(defun org2jekyll/get-publish-date! (orgfile)
   "Get the publication date from an ORGFILE."
-  (publish/get-option-from-file! orgfile "DATE"))
+  (org2jekyll/get-option-from-file! orgfile "DATE"))
 
-(defvar publish/map-keys '(("title" . "title")
-                           ("categories" . "categories")
-                           ("date" . "date")
-                           ("description" . "excerpt")
-                           ("author" . "author")
-                           ("layout" . "layout")
-                           ("blog" . "blog"))
+(defvar org2jekyll/map-keys '(("title" . "title")
+                              ("categories" . "categories")
+                              ("date" . "date")
+                              ("description" . "excerpt")
+                              ("author" . "author")
+                              ("layout" . "layout")
+                              ("blog" . "blog"))
   "Keys to map from org headers to jekyll's headers.")
 
-(defun publish/org-to-yaml-metadata (org-metadata)
+(defun org2jekyll/org-to-yaml-metadata (org-metadata)
   "Given an ORG-METADATA map, return a yaml one with transformed data."
-  (--map `(,(assoc-default (car it) publish/map-keys) . ,(cdr it)) org-metadata))
+  (--map `(,(assoc-default (car it) org2jekyll/map-keys) . ,(cdr it)) org-metadata))
 
-(defun publish/convert-timestamp-to-yyyy-dd-mm (timestamp)
+(defun org2jekyll/--convert-timestamp-to-yyyy-dd-mm (timestamp)
   "Convert org TIMESTAMP to ."
   (format-time-string "%Y-%m-%d"
                       (apply 'encode-time (org-parse-time-string timestamp))))
@@ -202,10 +203,10 @@ Depends on the metadata header blog."
 (require 'dash)
 (require 's)
 
-(defun publish/to-yaml-header (org-metadata)
+(defun org2jekyll/to-yaml-header (org-metadata)
   "Given a list of ORG-METADATA, compute the yaml header string."
   (--> org-metadata
-    publish/org-to-yaml-metadata
+    org2jekyll/org-to-yaml-metadata
     (--map (format "%s: %s" (car it) (cdr it)) it)
     (cons "---" it)
     (cons "#+BEGIN_HTML" it)
@@ -213,7 +214,7 @@ Depends on the metadata header blog."
     (-snoc it "#+END_HTML\n")
     (s-join "\n" it)))
 
-(defun publish/categories-csv-to-yaml (categories-csv)
+(defun org2jekyll/categories-csv-to-yaml (categories-csv)
   "Transform a CATEGORIES-CSV entries into a yaml entries."
   (->> categories-csv
     (concat ",")
@@ -222,18 +223,18 @@ Depends on the metadata header blog."
     s-trim
     (concat "\n")))
 
-(defun publish/publish-post! (&optional org-file)
+(defun org2jekyll/publish-post! (&optional org-file)
   "Publish a post ready for jekyll to render it."
   (interactive)
   (let ((orgfile (if org-file org-file (buffer-file-name (current-buffer)))))
-    (if (publish/article-p! orgfile)
-        (let* ((org-metadata             (publish/get-options-from-file! orgfile '("title" "date" "categories" "description" "author" "blog")))
-               (date                     (publish/convert-timestamp-to-yyyy-dd-mm (assoc-default "date" org-metadata)))
+    (if (org2jekyll/article-p! orgfile)
+        (let* ((org-metadata             (org2jekyll/get-options-from-file! orgfile '("title" "date" "categories" "description" "author" "blog")))
+               (date                     (org2jekyll/--convert-timestamp-to-yyyy-dd-mm (assoc-default "date" org-metadata)))
                (blog-project             (assoc-default "blog" org-metadata))
                (yaml-headers             `(("layout"      . "post")
                                            ("title"       . ,(assoc-default "title" org-metadata))
                                            ("date"        . ,date)
-                                           ("categories"  . ,(publish/categories-csv-to-yaml (assoc-default "categories" org-metadata)))
+                                           ("categories"  . ,(org2jekyll/categories-csv-to-yaml (assoc-default "categories" org-metadata)))
                                            ("author"      . ,(assoc-default "author" org-metadata))
                                            ("description" . ,(assoc-default "description" org-metadata))))
                (temp-org-jekyll-filename  (format "%s-%s" date (file-name-nondirectory orgfile)))
@@ -242,7 +243,7 @@ Depends on the metadata header blog."
           (with-temp-file temp-postfile                 ;; write temporary file updated with jekyll specifics
             (insert-file-contents orgfile)
             (goto-char (point-min))
-            (insert (publish/to-yaml-header yaml-headers)))
+            (insert (org2jekyll/to-yaml-header yaml-headers)))
           (org-publish-file temp-postfile (assoc blog-project org-publish-project-alist)) ;; publish the file with the right projects
           (delete-file temp-postfile))                  ;; remove the temporary file
       (message "This file is not an article, skip."))))
@@ -259,9 +260,9 @@ Depends on the metadata header blog."
 ;; directory to _post with the required date prefix in the filename), we
 ;; then need to html-export it to the jekyll rootdir (with org-publish).
 
-;; (global-set-key (kbd "C-c b n") 'jekyll-draft-post)
+;; (global-set-key (kbd "C-c b n") 'org2jekyll/create-draft-post)
 ;; (global-set-key (kbd "C-c b P") 'jekyll-publish-post)
-;; (global-set-key (kbd "C-c b p") 'jekyll-list-posts)
+;; (global-set-key (kbd "C-c b p") 'org2jekyll/list-posts)
 ;; (global-set-key (kbd "C-c b D") 'jekyll-list-drafs)
 
 (provide 'org2jekyll)
